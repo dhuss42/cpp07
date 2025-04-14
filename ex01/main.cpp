787315df42cac4a10505b548bf0c54914659f598
david <david.david@david.com>
2025-04-14 10:32:20 +0200

    added extra test for ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -13,28 +13,42 @@
 #include "iter.hpp"
 
 template <typename T>
-void	addOne(T &value)
+void	subOne(T &value)
 {
-	value++;
+	value--;
 }
 
 template <typename T>
 void	print(T &value)
 {
-	std::cout << value;
+	std::cout << value << " ";
 }
 
 int	main()
 {
 	std::cout << "\033[34m[----------Tests----------]\033[37m" << std::endl;
 	std::cout << "\033[33m- Int Array\033[37m" << std::endl;
-	std::cout << "\033[35m--> pre addOne\033[37m" << std::endl;
+	std::cout << "\033[35m--> pre subOne\033[37m" << std::endl;
 	int arr_int[5] = {1, 2, 3, 4, 5};
 	size_t len = 5;
 	iter(arr_int, len, print<int>);
 	std::cout << std::endl;
-	iter(arr_int, len, addOne<int>);
-	std::cout << "\033[35m--> post addOne\033[37m" << std::endl;
+	iter(arr_int, len, subOne<int>);
+	std::cout << "\033[35m--> post subOne\033[37m" << std::endl;
 	iter(arr_int, len, print<int>);
 	std::cout << std::endl;
+	std::cout << "\033[35m--> pre subOne\033[37m" << std::endl;
+
+
+	std::cout << "\n\033[33m- Float Array\033[37m" << std::endl;
+	std::cout << "\033[35m--> pre subOne\033[37m" << std::endl;
+	float	arr_float[5] = {1.5, 2.34, 3.7, 4.09, 5.1234};
+	double len_d = 5;
+	iter(arr_float, len, print<float>);
+	std::cout << std::endl;
+	iter(arr_float, len, subOne<float>);
+	std::cout << "\033[35m--> post subOne\033[37m" << std::endl;
+	iter(arr_float, len, print<float>);
+	std::cout << std::endl;
+	std::cout << "\033[35m--> pre subOne\033[37m" << std::endl;
 }
